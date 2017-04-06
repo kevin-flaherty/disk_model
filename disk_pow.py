6ce96603052d11e01ba5de096bf30300ea0ff55b
Kevin Flaherty <kevco46@gmail.com>
2017-04-06 16:58:01 -0400

    Adjustment to disk.py for nearly edge-on orientations

--- disk_pow.py
+++ disk_pow.py
@@ -255,7 +255,10 @@ class Disk:
         ### Start of Radiative Transfer portion of the code...
         # Define and initialize cylindrical grid
         Smin = 1*Disk.AU                 # offset from zero to log scale
-        Smax = 2.*self.zmax/self.costhet       # los distance through disk
+        if self.thet > np.arctan(self.Rout/self.zmax):
+            Smax = 2*self.Rout/self.sinthet
+        else:
+            Smax = 2.*self.zmax/self.costhet       # los distance through disk
         Smid = Smax/2.                    # halfway along los
         ytop = Smax*self.sinthet/2.       # y origin offset for observer xy center
         R = np.linspace(0,self.Rout,self.nr)#np.logspace(np.log10(self.Rin),np.log10(self.Rout),self.nr)
@@ -274,6 +277,8 @@ class Disk:
 
         # transform grid
         tdiskZ = self.zmax*(np.ones((self.nphi,self.nr,self.nz)))-self.costhet*S
+        if self.thet > np.arctan(self.Rout/self.zmax):
+            tdiskZ -=(Y*self.sinthet).repeat(self.nz).reshape(self.nphi,self.nr,self.nz)
         tdiskY = ytop - self.sinthet*S + (Y/self.costhet).repeat(self.nz).reshape(self.nphi,self.nr,self.nz)
         tr = np.sqrt(X.repeat(self.nz).reshape(self.nphi,self.nr,self.nz)**2+tdiskY**2)
         notdisk = tr > self.Rout  # - individual grid elements not in disk
